3f05dc8e64b0528998fcc93efc9ae9a1ab302988
Milagros-Trejo <82959873+Milagros-Trejo@users.noreply.github.com>
2023-11-05 19:26:28 -0300

    Iniciar proyecto Laravel

---
+++ app/Models/Incidente.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Incidente extends Model
+{
+    use HasFactory;
+
+    protected $table = 'incidente';
+    public $timestamps = false;
+
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'fechaIncidente',
+        'horaDeApertura',
+        'horaDeCierre',
+        'observaciones'
+    ]
+
+    protected $guarded = [
+        
+    ]
+}
