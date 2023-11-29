ace43f30f4a103fa7a5d44debc4838c8666eac5d
Milagros-Trejo <82959873+Milagros-Trejo@users.noreply.github.com>
2023-11-29 12:03:45 -0300

    Vista hecha

--- resources/views/incidentes/index.blade.php
+++ resources/views/incidentes/index.blade.php
@@ -1,14 +1,20 @@
-<html>
-<h1>Index</h1>
+@extends('incidentes.base')
+@section('contenido')
+<h1 style="color: #581ee1">Listado de incidentes activos</h1>
 <table>
     @foreach($incidentes as $current)
-    <tr>
-        <td>{{$current->observaciones}}</td>
-        <td>{{$current->miembro->nicknameMiembro}}</td>
-        <td>{{$current->comunidad->descripcion}}</td>
-        <td>{{$current->prestacionservicio->servicioprestado->descripcion}}</td>
-        <td>{{$current->prestacionservicio->establecimiento->nombre}}</td>
-    </tr>
+    <div class="list-group">
+        <a class="list-group-item m-2">
+            <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1" style="color: #581ee1">
+                    {{$current->prestacionservicio->establecimiento->nombre}} - {{$current->prestacionservicio->servicioprestado->descripcion}}
+                </h5>
+                <small>{{$current->comunidad->descripcion}}</small>
+            </div>
+            <p class="mb-1">{{$current->observaciones}}</p>
+            <small>Reportante: {{$current->miembro->nicknameMiembro}}</small>
+        </a>
+    </div>
     @endforeach
 </table>
-</html> 
+@endsection
